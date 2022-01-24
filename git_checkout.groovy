4beeb387ccd44735524827036558f59fae5a6a21
priyasoundh <32993324+priyasoundh@users.noreply.github.com>
2022-01-24 16:24:12 +0530

    Update git_checkout.groovy

--- git_checkout.groovy
+++ git_checkout.groovy
@@ -3,7 +3,7 @@ node
   stage ("git checkout")
   {
     println "cloning started"
-    bat "git clone -b main https://github.com/priyasoundh/shared_library.git"
+    sh "git clone -b main https://github.com/priyasoundh/shared_library.git"
     println "repo cloned"
   }
 }
